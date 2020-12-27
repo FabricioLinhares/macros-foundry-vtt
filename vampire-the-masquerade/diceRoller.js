afe929039a47d765f11c304fa2a017add33b9943
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2020-12-27 17:38:39 -0300

    add roll mode to vampire diceRoller

--- vampire-the-masquerade/diceRoller.js
+++ vampire-the-masquerade/diceRoller.js
@@ -38,10 +38,23 @@ const getFailures = results => {
 	return failure;
 }
 
+const sendRollMessage = (roll, {content, rollMode}) => {
+	const currentRollMode = game.settings.get('core', 'rollMode');
+
+	game.settings.set('core', 'rollMode', rollMode);
+
+	roll.toMessage({
+		flavor: content,
+	});
+
+	game.settings.set('core', 'rollMode', currentRollMode);
+}
+
 const diceRoll = container => {
-	const ammount = Number(container[0].querySelector('#dice-ammount').value)
-	const difficulty = Number(container[0].querySelector('#dice-difficulty').value)
-	const additional = Number(container[0].querySelector('#dice-additional').value)
+	const ammount = Number(container[0].querySelector('#dice-ammount').value);
+	const difficulty = Number(container[0].querySelector('#dice-difficulty').value);
+	const additional = Number(container[0].querySelector('#dice-additional').value);
+	const rollMode = container[0].querySelector('#chat-type').selectedOptions[0].value;
 	   
 	const roll = new Roll(`${ammount + additional}d10cs>=${difficulty}`);
 	roll.roll();
@@ -50,23 +63,44 @@ const diceRoll = container => {
 	const failure = getFailures(roll.terms[0].results);
 	const resultType = getResultType(success, failure);
 
-	roll.toMessage({
-		flavor: `
-			<h2><strong>Resultado: ${success - failure} (${resultType})</strong></h2>
-			<strong>Sucessos:</strong> ${success} </br>
-			<strong>Falhas:</strong> ${failure}
-		`,
-	});
+	const content = `
+		<h2><strong>Resultado: ${success - failure} (${resultType})</strong></h2>
+		<strong>Sucessos:</strong> ${success} </br>
+		<strong>Falhas:</strong> ${failure}
+	`
+
+	const data = {content, rollMode}
+	
+	sendRollMessage(roll, data);
 }
 
 const options = {
 	title: "Rolador de Dados",
 	content: `
-		<label for='dice-ammount'>Quantidade</label> <input type='number' id='dice-ammount' min='1' max='10' value='1'></input>
+		<div class="input-block">
+			<label for='dice-ammount'>Quantidade</label>
+			<input type='number' id='dice-ammount' min='1' max='10' value='1'></input>
+		</div>
 		<hr>
-		<label for='dice-difficulty'>Dificuldade</label> <input type='number' id='dice-difficulty' min='3' max='10' value='6'></input>
+		<div class="input-block">
+			<label for='dice-difficulty'>Dificuldade</label>
+			<input type='number' id='dice-difficulty' min='3' max='10' value='6'></input>
+		</div>
 		<hr>
-		<label for='dice-additional'>Modificador</label> <input type='number' id='dice-additional' value='0'></input>
+		<div class="input-block">
+			<label for='dice-additional'>Modificador</label>
+			<input type='number' id='dice-additional' value='0'></input>
+		</div>
+		<hr>
+		<div class="select-block">
+			<label for='chat-type'>Tipo de Rolagem: </label>
+			<select id='chat-type'>
+				<option value='roll' selected>Pública</option>
+				<option value='gmroll'>Privada para o GM</option>
+				<option value='blindroll'>Apenas GM</option>
+				<option value='selfroll'>Apenas eu</option>
+			</select>
+		</div>
 		<hr>
 	`,
 	label: "Rolar",
